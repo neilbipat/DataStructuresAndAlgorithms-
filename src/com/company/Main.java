3c604baeea4a3e156e8186462d6d28b89d945e9c
Neil BipatE <neilbipat@gmail.com>
2020-02-07 16:48:51 -0500

    Moved to the Main file and created the new LinkedList along with the head

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	LinkedList linkedList = new LinkedList();
+
+	/*Creating the Head Node*/
+        Node first = new Node(1);
+        linkedList.head = first;
     }
 }
