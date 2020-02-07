11128ae37320f696d1aeb570fe4aeaf2019e61fe
Neil BipatE <neilbipat@gmail.com>
2020-02-07 16:53:30 -0500

    In LinkedList file created the printList function to print elements out to console

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -3,6 +3,15 @@ package com.company;
 public class LinkedList {
     LinkedList(){};
     Node head;
+
+    /*Printg Elements in the List*/
+    public void printList(){
+        Node var = head;
+        while(var != null){
+            System.out.println(var.data);
+            var = var.next;
+        }
+    }
 }
 
 
